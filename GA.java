73e4ab8aea45db497955712b7d9ee97046657929
黄华姿 <74940465+huanghuazi@users.noreply.github.com>
2020-11-26 19:16:15 +0800

    采用遗传算法求解函数最优值

--- GA.java
+++ GA.java
@@ -1,16 +1,8 @@
 package com.company;
 
-//public class Main {
-//
-//    public static void main(String[] args) {
-//	// write your code here
-//    }
-//}
-
 import java.util.ArrayList;
         import java.util.Arrays;
         import java.util.List;
-//import java.util.Scanner;
 
 public class GA {
     static int geneLength = 22;
